b259200a94538a8807b4a9bf1f31fcb0abd32bb6
Almann Goo <almann@amazon.com>
2016-10-07 11:50:19 -0700

    Cleans up imports in REPL.

--- src/com/amazon/ionsql/tools/REPL.kt
+++ src/com/amazon/ionsql/tools/REPL.kt
@@ -15,7 +15,6 @@ import java.io.FileInputStream
 import java.io.FileOutputStream
 import java.io.OutputStream
 import java.util.*
-import java.util.concurrent.TimeUnit
 import java.util.concurrent.TimeUnit.MILLISECONDS
 import java.util.concurrent.TimeUnit.NANOSECONDS
 
