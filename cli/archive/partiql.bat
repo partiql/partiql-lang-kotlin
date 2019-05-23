6f795349a156dd97634013f041399b08e912b0b9
barbosf <barbosf@amazon.com>
2019-05-22 17:24:03 -0700

    Checks for java and exits if no JDK installed
    Checks for java first in JAVA_HOME then in PATH. If doesn't encounter it
    errors out with a link to install openjdk

--- cli/archive/partiql.bat
+++ cli/archive/partiql.bat
@@ -2,7 +2,15 @@
 
 SET base_dir=%~dp0
 
-SET java_exec=java
-IF DEFINED JAVA_HOME (SET java_exec=%JAVA_HOME%/bin/java)
+IF DEFINED JAVA_HOME (SET java_exec=%JAVA_HOME%\bin\java)
+ELSE ( 
+    WHERE java >nul 2>nul         
+    IF %ERRORLEVEL% NEQ 0 (
+        echo Java not found! Please install the Java JDK https://openjdk.java.net/
+        PAUSE
+        exit \b 
+    )
+    SET java_exec=java    
+)
 
-"%java_exec%" -jar %base_dir%\partiql-cli.jar
+"%java_exec%" -jar "%base_dir%\partiql-cli.jar"
