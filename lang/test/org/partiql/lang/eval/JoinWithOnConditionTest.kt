62abc318ba40248e6c3cb2eed7e98f191044296b
Alan Cai <caialan@amazon.com>
2020-09-04 14:11:40 -0400

    Add non-associative join paren test

--- lang/test/org/partiql/lang/eval/JoinWithOnConditionTest.kt
+++ lang/test/org/partiql/lang/eval/JoinWithOnConditionTest.kt
@@ -69,6 +69,12 @@ class JoinWithOnConditionTest : EvaluatorTestBase() {
                                    t2.val AS val2
                             FROM t1 AS t1 JOIN t2 as t2 ON t1.id = t2.id
                         """
+
+    private val session = mapOf(
+            "A" to "[ { 'n': 1 }, { 'n': 3 } ]",
+            "B" to "[ { 'n': 1 }, { 'n': 2 }, { 'n': 3 } ]",
+            "C" to "[ { 'n': 2 }, { 'n': 3 } ]").toSession()
+
     @Test
     @Parameters
     fun joinWithOnConditionTest(pair: Pair<EvaluatorTestCase, EvaluationSession>): Unit =
@@ -109,5 +115,25 @@ class JoinWithOnConditionTest : EvaluatorTestBase() {
                             <<>>
                         """), sessionNullTableRow)
         )
+    }
+
+    @Test
+    fun naturalOrderJoinNonAssociativeTest() {
+        val testCase =
+            EvaluatorTestCase(
+                query = "SELECT * FROM A LEFT JOIN B ON A.n=B.n INNER JOIN C ON B.n=C.n",
+                expectedSql = """<< { 'n': 3, 'n': 3, 'n': 3 } >>"""
+            )
+        runTestCase(testCase, session)
+    }
+
+    @Test
+    fun specifiedOrderJoinNonAssociativeTest() {
+        val testCase =
+            EvaluatorTestCase(
+                query = "SELECT * FROM A LEFT JOIN (B INNER JOIN C ON B.n=C.n) ON A.n=B.n",
+                expectedSql = """<< { 'n': 2, 'n': 2, '_3': NULL }, { 'n': 3, 'n': 3, 'n': 3 } >>"""
+            )
+        runTestCase(testCase, session)
     }
 }
