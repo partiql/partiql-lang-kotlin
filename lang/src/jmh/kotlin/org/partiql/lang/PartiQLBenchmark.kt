b70c9ff20503e9b24edfc0a73ebfbdb6302c2e24
Abhilash Kuhikar <kuhikar@amazon.com>
2021-09-10 12:27:50 -0700

    Adds non-trivial PartiQL benchmarks.

--- lang/src/jmh/kotlin/org/partiql/lang/PartiQLBenchmark.kt
+++ lang/src/jmh/kotlin/org/partiql/lang/PartiQLBenchmark.kt
@@ -11,6 +11,7 @@ import org.openjdk.jmh.annotations.Setup
 import org.openjdk.jmh.annotations.State
 import org.openjdk.jmh.annotations.TearDown
 import org.openjdk.jmh.infra.Blackhole
+import org.partiql.lang.eval.EvaluationSession
 import org.partiql.lang.syntax.SqlParser
 import java.util.concurrent.TimeUnit
 
@@ -65,7 +66,47 @@ open class PartiQLBenchmark {
         var a = 1
         var b = 2
         var sum = 0
-        val parser = SqlParser(IonSystemBuilder.standard().build())
+
+        val ion = IonSystemBuilder.standard().build()
+        val parser = SqlParser(ion)
+        val pipeline = CompilerPipeline.standard(ion)
+
+        val data = """
+            { 
+                'hr': { 
+                    'employeesNestScalars': <<
+                        { 
+                            'id': 3, 
+                            'name': 'Bob Smith', 
+                            'title': null, 
+                            'projects': [ 
+                                'AWS Redshift Spectrum querying',
+                                'AWS Redshift security',
+                                'AWS Aurora security'
+                            ]
+                        },
+                        { 
+                            'id': 4, 
+                            'name': 'Susan Smith', 
+                            'title': 'Dev Mgr', 
+                            'projects': []
+                        },
+                        { 
+                            'id': 6, 
+                            'name': 'Jane Smith', 
+                            'title': 'Software Eng 2', 
+                            'projects': [ 'AWS Redshift security' ]
+                        }
+                    >>
+                } 
+            }
+        """.trimIndent()
+        val bindings = pipeline.compile(parser.parseExprNode(data)).eval(EvaluationSession.standard()).bindings
+        val session = EvaluationSession.build { globals(bindings) }
+
+        val query = "SELECT * FROM hr.employeesNestScalars"
+        val exprNode = parser.parseExprNode(query)
+        val expression = pipeline.compile(exprNode)
     }
 
     /**
@@ -91,12 +132,32 @@ open class PartiQLBenchmark {
     }
 
     /**
-     * Example PartiQL benchmark
+     * Example PartiQL benchmark for parsing a query
      */
     @Benchmark
     @BenchmarkMode(Mode.AverageTime)
     fun testPartiQLParser(state: MyState2, blackhole: Blackhole) {
-        val expr = state.parser.parseExprNode("SELECT * FROM foo")
+        val expr = state.parser.parseExprNode(state.query)
         blackhole.consume(expr)
+    }
+
+    /**
+     * Example PartiQL benchmark for compiling a query
+     */
+    @Benchmark
+    @BenchmarkMode(Mode.AverageTime)
+    fun testPartiQLCompiler(state: MyState2, blackhole: Blackhole) {
+        val exprValue = state.pipeline.compile(state.exprNode)
+        blackhole.consume(exprValue)
+    }
+
+    /**
+     * Example PartiQL benchmark for evaluating a query
+     */
+    @Benchmark
+    @BenchmarkMode(Mode.AverageTime)
+    fun testPartiQLEvaluator(state: MyState2, blackhole: Blackhole) {
+        val exprValue = state.expression.eval(state.session)
+        blackhole.consume(exprValue)
     }
 }
