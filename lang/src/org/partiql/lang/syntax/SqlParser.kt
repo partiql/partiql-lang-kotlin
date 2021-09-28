dab08fc2fbe356f740acf828eca4cbc21c57f6b1
Ziqi Li <lziq@amazon.com>
2021-09-28 10:07:02 -0700

    Added curly braces for line 2419 in SqlParser

--- lang/src/org/partiql/lang/syntax/SqlParser.kt
+++ lang/src/org/partiql/lang/syntax/SqlParser.kt
@@ -2416,10 +2416,12 @@ class SqlParser(private val ion: IonSystem) : Parser {
         }
         // For "TIME WITH TIME ZONE", if the time zone is not explicitly specified, we still consider it as valid.
         // We will add the default time zone to it later in the evaluation phase.
-        if (!withTimeZone || timeWithoutTimeZoneRegex.matches(timeString))
-            tryTimeParsing (timeString, DateTimeFormatter.ISO_TIME, LocalTime::parse)
-        else
-            tryTimeParsing (timeString, DateTimeFormatter.ISO_TIME, OffsetTime::parse)
+        if (!withTimeZone || timeWithoutTimeZoneRegex.matches(timeString)) {
+            tryTimeParsing(timeString, DateTimeFormatter.ISO_TIME, LocalTime::parse)
+        }
+        else {
+            tryTimeParsing(timeString, DateTimeFormatter.ISO_TIME, OffsetTime::parse)
+        }
 
         // Extract the precision from the time string representation if the precision is not specified.
         // For e.g., TIME '23:12:12.12300' should have precision of 5.
