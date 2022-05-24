125b1700772191b3ef49e53c54d48fb57190d186
John Quinn <johqunn@amazon.com>
2022-05-24 15:54:28 -0700

    Add integration tests for  function of CLI

---
+++ cli/test/org/partiql/cli/CliTestUtility.kt
@@ -0,0 +1,60 @@
+package org.partiql.cli
+
+import com.amazon.ion.IonSystem
+import com.amazon.ion.system.IonSystemBuilder
+import org.junit.Assert
+import org.partiql.lang.CompilerPipeline
+import org.partiql.lang.eval.Bindings
+import org.partiql.lang.eval.ExprValue
+import org.partiql.lang.eval.ExprValueFactory
+import java.io.OutputStream
+
+/**
+ * A helper class to initialize the internal-scoped [Cli] and run queries for the purpose of unit and integration
+ * testing.
+ */
+class CliTestUtility() {
+
+    companion object {
+
+        /**
+         * Initializes a CLI and runs the passed-in query
+         */
+        fun makeCliAndGetResult(
+            query: String,
+            input: String? = null,
+            bindings: Bindings<ExprValue> = Bindings.empty(),
+            outputFormat: OutputFormat = OutputFormat.ION_TEXT,
+            output: OutputStream = OutputStream.nullOutputStream(),
+            ion: IonSystem = IonSystemBuilder.standard().build(),
+            compilerPipeline: CompilerPipeline = CompilerPipeline.standard(ion),
+            valueFactory: ExprValueFactory = ExprValueFactory.standard(ion)
+        ): String {
+            val cli = Cli(
+                valueFactory,
+                input?.byteInputStream(Charsets.UTF_8) ?: EmptyInputStream(),
+                output,
+                outputFormat,
+                compilerPipeline,
+                bindings,
+                query
+            )
+            cli.run()
+            return output.toString()
+        }
+
+        /**
+         * An assertion helper
+         */
+        fun assertAsIon(expected: String, actual: String) {
+            val ion = IonSystemBuilder.standard().build()
+            assertAsIon(ion, expected, actual)
+        }
+
+        /**
+         * An assertion helper
+         */
+        fun assertAsIon(ion: IonSystem, expected: String, actual: String) =
+            Assert.assertEquals(ion.loader.load(expected), ion.loader.load(actual))
+    }
+}
