fbf2b53e72324236f15d090a69bb68f5fd9367f5
Ziqi Li <lziq@amazon.com>
2022-05-31 23:09:25 -0700

    Further make ktlint happy

--- lang/src/org/partiql/lang/typesystem/builtin/types/primitive/TimeType.kt
+++ lang/src/org/partiql/lang/typesystem/builtin/types/primitive/TimeType.kt
@@ -3,7 +3,10 @@ package org.partiql.lang.typesystem.builtin.types.primitive
 import org.partiql.lang.eval.ExprValue
 import org.partiql.lang.eval.ExprValueType
 import org.partiql.lang.typesystem.builtin.types.valueFactory
-import org.partiql.lang.typesystem.interfaces.type.*
+import org.partiql.lang.typesystem.interfaces.type.BuiltInType
+import org.partiql.lang.typesystem.interfaces.type.ParametricType
+import org.partiql.lang.typesystem.interfaces.type.SqlType
+import org.partiql.lang.typesystem.interfaces.type.TypeParameters
 import org.partiql.lang.util.asIonInt
 
 object TimeType : BuiltInType(), ParametricType {
