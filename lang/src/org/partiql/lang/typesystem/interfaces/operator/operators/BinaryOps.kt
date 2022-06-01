4efba07a8453a139d4293f82e7948aba06213fff
lziq <67429381+lziq@users.noreply.github.com>
2022-06-01 13:38:24 -0700

    Ots interfaces  (#620)

--- lang/src/org/partiql/lang/typesystem/interfaces/operator/operators/BinaryOps.kt
+++ lang/src/org/partiql/lang/typesystem/interfaces/operator/operators/BinaryOps.kt
@@ -1,11 +1,13 @@
-package org.partiql.lang.typesystem.interfaces.operator
+package org.partiql.lang.typesystem.interfaces.operator.operators
 
 import org.partiql.lang.eval.ExprValue
+import org.partiql.lang.typesystem.interfaces.operator.OpAlias
+import org.partiql.lang.typesystem.interfaces.operator.SqlOperator
 import org.partiql.lang.typesystem.interfaces.type.SqlType
 import org.partiql.lang.typesystem.interfaces.type.SqlTypeWithParameters
 import org.partiql.lang.typesystem.interfaces.type.TypeParameters
 
-abstract class BinaryOp internal constructor() : SqlOperator {
+sealed class BinaryOp(override val operatorAlias: OpAlias) : SqlOperator {
     /**
      * Type of left-hand side expression
      */
@@ -40,3 +42,21 @@ abstract class BinaryOp internal constructor() : SqlOperator {
         val parametersOfRhsType: TypeParameters
     )
 }
+
+abstract class AndOp : BinaryOp(OpAlias.AND)
+abstract class ConcatOp : BinaryOp(OpAlias.CONCAT)
+abstract class DivideOp : BinaryOp(OpAlias.DIVIDE)
+abstract class EqOp : BinaryOp(OpAlias.EQ)
+abstract class ExceptOp : BinaryOp(OpAlias.EXCEPT)
+abstract class GteOp : BinaryOp(OpAlias.GTE)
+abstract class GtOp : BinaryOp(OpAlias.GT)
+abstract class IntersectOp : BinaryOp(OpAlias.INTERSECT)
+abstract class LteOp : BinaryOp(OpAlias.LTE)
+abstract class LtOp : BinaryOp(OpAlias.LT)
+abstract class MinusOp : BinaryOp(OpAlias.MINUS)
+abstract class ModuloOp : BinaryOp(OpAlias.MODULO)
+abstract class NeOp : BinaryOp(OpAlias.NE)
+abstract class OrOp : BinaryOp(OpAlias.OR)
+abstract class PlusOp : BinaryOp(OpAlias.PLUS)
+abstract class TimesOp : BinaryOp(OpAlias.TIMES)
+abstract class UnionOp : BinaryOp(OpAlias.UNION)
