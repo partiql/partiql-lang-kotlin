8d220672ae1d8588bf704c55ea94701a43aec30f
Yingtao Liu <yliuuu@amazon.com>
2022-07-12 16:23:27 -0700

    added check for int overflow

--- lang/src/org/partiql/lang/eval/builtins/MathFunctions.kt
+++ lang/src/org/partiql/lang/eval/builtins/MathFunctions.kt
@@ -1,16 +1,26 @@
 package org.partiql.lang.eval.builtins
 
+import org.partiql.lang.errors.ErrorCode
 import org.partiql.lang.eval.EvaluationSession
 import org.partiql.lang.eval.ExprFunction
 import org.partiql.lang.eval.ExprValue
 import org.partiql.lang.eval.ExprValueFactory
+import org.partiql.lang.eval.errIntOverflow
+import org.partiql.lang.eval.errNoContext
 import org.partiql.lang.eval.numberValue
 import org.partiql.lang.types.FunctionSignature
 import org.partiql.lang.types.StaticType
+import org.partiql.lang.util.bigDecimalOf
+import org.partiql.lang.util.compareTo
+import org.partiql.lang.util.div
+import org.partiql.lang.util.plus
+import org.partiql.lang.util.times
+import org.partiql.lang.util.unaryMinus
 import java.lang.Double.NEGATIVE_INFINITY
 import java.lang.Double.NaN
 import java.lang.Double.POSITIVE_INFINITY
 import java.math.BigDecimal
+import java.math.BigInteger
 import java.math.RoundingMode
 
 /**
@@ -45,25 +55,40 @@ private class UnaryNumeric(
         returnType = StaticType.NUMERIC,
     )
 
-    override fun callWithRequired(session: EvaluationSession, required: List<ExprValue>): ExprValue {
-        return when (val n = function.invoke(required.first().numberValue())) {
-            is Double, is Float -> valueFactory.newFloat(n.toDouble())
-            is Int -> valueFactory.newInt(n.toInt())
-            is Long -> valueFactory.newInt(n.toLong())
-            else -> valueFactory.newFloat(n.toDouble())
-        }
-    }
+    override fun callWithRequired(session: EvaluationSession, required: List<ExprValue>): ExprValue =
+        function.invoke(required.first().numberValue()).exprValue(valueFactory)
 }
 
 private fun ceil(n: Number): Number = when (n) {
     POSITIVE_INFINITY, NEGATIVE_INFINITY, NaN -> n
     // support for numbers that are larger than 64 bits.
-    is BigDecimal -> n.setScale(0, RoundingMode.CEILING).toInt()
-    else -> kotlin.math.ceil(n.toDouble()).toInt()
+    else -> transformIntType(bigDecimalOf(n).setScale(0, RoundingMode.CEILING).toBigIntegerExact())
 }
 
 private fun floor(n: Number): Number = when (n) {
     POSITIVE_INFINITY, NEGATIVE_INFINITY, NaN -> n
-    is BigDecimal -> n.setScale(0, RoundingMode.FLOOR).toInt()
-    else -> kotlin.math.floor(n.toDouble()).toInt()
+    else -> transformIntType(bigDecimalOf(n).setScale(0, RoundingMode.FLOOR).toBigIntegerExact())
+}
+
+// wrapper for transform function result to corresponding integer type
+private fun transformIntType(n: BigInteger): Number = when (n) {
+    in Int.MIN_VALUE.toBigInteger()..Int.MAX_VALUE.toBigInteger() -> n.toInt()
+    in Long.MIN_VALUE.toBigInteger()..Long.MAX_VALUE.toBigInteger() -> n.toLong()
+    /**
+     * currently kotlin-lang-ParitQL did not support integer value bigger than 64 bits.
+     */
+    else -> errIntOverflow(8)
+}
+
+// wrapper for converting result to expression value
+private fun Number.exprValue(valueFactory: ExprValueFactory): ExprValue = when (this) {
+    is Int -> valueFactory.newInt(this)
+    is Long -> valueFactory.newInt(this)
+    is Double, is Float -> valueFactory.newFloat(this.toDouble())
+    is BigDecimal -> valueFactory.newDecimal(this)
+    else -> errNoContext(
+        "Cannot convert number to expression value: $this",
+        errorCode = ErrorCode.EVALUATOR_INVALID_CONVERSION,
+        internal = true
+    )
 }
