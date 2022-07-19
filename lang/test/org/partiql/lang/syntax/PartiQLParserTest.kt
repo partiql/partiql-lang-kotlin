44d62523f8b502aa09b28b2d3f58d27af79734bc
John Ed Quinn <johqunn@amazon.com>
2022-07-19 07:38:23 -0700

    Add additional JOIN test

--- lang/test/org/partiql/lang/syntax/PartiQLParserTest.kt
+++ lang/test/org/partiql/lang/syntax/PartiQLParserTest.kt
@@ -54,6 +54,7 @@ class PartiQLParserTest {
         }
     }
 
+    // TODO: Delete
     @Test
     fun testVisual() {
         val query = "SELECT * FROM a" // LEFT JOIN b ON c;"
@@ -107,7 +108,8 @@ class PartiQLParserTest {
                 "SELECT * FROM a FULL OUTER JOIN c ON id",
                 "SELECT * FROM a FULL JOIN c ON id",
                 "SELECT * FROM a FULL JOIN (b FULL JOIN c ON d) ON e",
-                "SELECT x FROM A INNER JOIN (B INNER JOIN (C INNER JOIN D ON C = D) ON B = C) ON A = B"
+                "SELECT x FROM A INNER JOIN (B INNER JOIN (C INNER JOIN D ON C = D) ON B = C) ON A = B",
+                "SELECT x FROM A INNER JOIN (B RIGHT OUTER JOIN (C INNER JOIN D ON C = D) ON B = C) ON A = B"
             )
             return queries
         }
