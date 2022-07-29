0b4540f474caff7dba1d6c327e0e85012402829b
David Lurton <dlurton@users.noreply.github.com>
2022-07-29 13:57:36 -0700

    Refactor/improve planner integration tests (#686)
    Refactors and improves the query planner integration tests. Namely, this includes the new FilterScanToKeyLookup optimization, but also renames a few things to make them more generic--the integration tests are not only for DML anymore.

--- lang/test/org/partiql/lang/planner/Util.kt
+++ lang/test/org/partiql/lang/planner/Util.kt
@@ -12,7 +12,6 @@ import org.partiql.lang.ast.SourceLocationMeta
 import org.partiql.lang.domains.PartiqlPhysical
 import org.partiql.lang.errors.Problem
 import org.partiql.lang.errors.ProblemDetails
-import org.partiql.lang.eval.BindingName
 import org.partiql.lang.util.SexpAstPrettyPrinter
 
 /**
@@ -21,13 +20,11 @@ import org.partiql.lang.util.SexpAstPrettyPrinter
  * The fake unique identifier of bound variables is computed to be `fake_uid_for_${globalVariableName}`.
  */
 fun createFakeGlobalsResolver(vararg globalVariableNames: Pair<String, String>) =
-    object : GlobalVariableResolver {
-        override fun resolveGlobal(bindingName: BindingName): GlobalResolutionResult {
-            val matches = globalVariableNames.filter { bindingName.isEquivalentTo(it.first) }
-            return when (matches.size) {
-                0 -> GlobalResolutionResult.Undefined
-                else -> GlobalResolutionResult.GlobalVariable(matches.first().second)
-            }
+    GlobalVariableResolver { bindingName ->
+        val matches = globalVariableNames.filter { bindingName.isEquivalentTo(it.first) }
+        when (matches.size) {
+            0 -> GlobalResolutionResult.Undefined
+            else -> GlobalResolutionResult.GlobalVariable(matches.first().second)
         }
     }
 
