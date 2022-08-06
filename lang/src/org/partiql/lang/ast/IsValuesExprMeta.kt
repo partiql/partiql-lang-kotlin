64bd59b66cdbf592ecdb4a50be07ed6402fcc2cd
John Ed Quinn <johqunn@amazon.com>
2022-08-05 17:50:41 -0700

    Adds support for implicit LIST on RHS of IN Predicate

---
+++ lang/src/org/partiql/lang/ast/IsValuesExprMeta.kt
@@ -0,0 +1,25 @@
+/*
+ * Copyright 2019 Amazon.com, Inc. or its affiliates.  All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ *  You may not use this file except in compliance with the License.
+ * A copy of the License is located at:
+ *
+ *      http://aws.amazon.com/apache2.0/
+ *
+ *  or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific
+ *  language governing permissions and limitations under the License.
+ */
+package org.partiql.lang.ast
+
+class IsValuesExprMeta private constructor() : Meta {
+    override val tag = TAG
+
+    companion object {
+        const val TAG = "\$is_values_expr"
+
+        val instance = IsValuesExprMeta()
+        val deserializer = MemoizedMetaDeserializer(TAG, instance)
+    }
+}
