65c66887c979e952b40f89258ea2fba782834eff
John Ed Quinn <johqunn@amazon.com>
2022-08-09 15:12:10 -0700

    Fixes tests and allows multiple matches in parenthesized MATCH

--- lang/test/org/partiql/lang/syntax/SqlParserMatchTest.kt
+++ lang/test/org/partiql/lang/syntax/SqlParserMatchTest.kt
@@ -775,7 +775,7 @@ class SqlParserMatchTest : SqlParserTestBase() {
 
     @Test
     fun prefilters() = assertExpressionNoRoundTrip(
-        "SELECT u as banCandidate FROM g MATCH (p:Post Where p.isFlagged = true) <-[:createdPost]- (u:User WHERE u.isBanned = false AND u.karma < 20) -[:createdComment]->(c:Comment WHERE c.isFlagged = true) WHERE p.title LIKE '%considered harmful%'",
+        "SELECT u as banCandidate FROM g MATCH (p:Post Where p.isFlagged = true) <-[:createdPost]- (u:Usr WHERE u.isBanned = false AND u.karma < 20) -[:createdComment]->(c:Comment WHERE c.isFlagged = true) WHERE p.title LIKE '%considered harmful%'",
     ) {
         PartiqlAst.build {
             select(
@@ -800,7 +800,7 @@ class SqlParserMatchTest : SqlParserTestBase() {
                                     ),
                                     node(
                                         variable = "u",
-                                        label = listOf("User"),
+                                        label = listOf("Usr"),
                                         prefilter = and(
                                             eq(
                                                 path(id("u"), pathExpr(lit(ionString("isBanned")), caseInsensitive())),
@@ -1054,7 +1054,7 @@ class SqlParserMatchTest : SqlParserTestBase() {
 
     @Test
     fun matchAndJoinCommas() {
-        assertFailsWith<ParserException> {
+        assertFailsWith<PartiQLParser.ParseErrorListener.ParseException> {
             assertExpressionNoRoundTrip(
                 "SELECT a,b,c, t1.x as x, t2.y as y FROM graph MATCH (a) -> (b), (a) -> (c), table1 as t1, table2 as t2",
             ) {
