fed114308ee7a728d0cc55c0261f7ff5887f11c3
Yingtao Liu <yliuuu@amazon.com>
2022-10-17 14:58:14 -0700

    resolve conflict

--- lang/src/org/partiql/lang/eval/EvaluatingCompiler.kt
+++ lang/src/org/partiql/lang/eval/EvaluatingCompiler.kt
@@ -442,7 +442,6 @@ internal class EvaluatingCompiler(
             is PartiqlAst.Expr.BagOp -> compileBagOp(expr, metas)
 
             is PartiqlAst.Expr.GraphMatch -> TODO("Compilation of GraphMatch expression")
-
             is PartiqlAst.Expr.CallWindow -> TODO("Evaluating Compiler doesn't support window function")
         }
     }
