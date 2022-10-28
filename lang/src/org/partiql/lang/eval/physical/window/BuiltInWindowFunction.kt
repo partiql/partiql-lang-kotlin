cb1b330a2142a25a2eab486791ba27c227861b52
Yingtao Liu <yliuuu@amazon.com>
2022-10-28 11:31:16 -0700

    clean up

--- lang/src/org/partiql/lang/eval/physical/window/BuiltInWindowFunction.kt
+++ lang/src/org/partiql/lang/eval/physical/window/BuiltInWindowFunction.kt
@@ -1,11 +1,5 @@
 package org.partiql.lang.eval.physical.window
 
-// Type Influencer for window function is not implemented yet.
-// internal fun createBuiltInWindowFunctionSignatures(): Map<String, WindowFunctionSignature> =
-//    createBuiltinWindowFunctions(ExprValueFactory.standard(IonSystemBuilder.standard().build()))
-//        .map { it.signature }
-//        .associateBy{ it.name }
-
 internal fun createBuiltinWindowFunctions() =
     listOf(
         Lag(),
