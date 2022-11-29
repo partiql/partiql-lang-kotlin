b4c81335f432646ca667cce08edb859ddeb1d7c7
Yingtao Liu <yliuuu@amazon.com>
2022-11-29 14:59:10 -0800

    remove pts and testscript

--- test/partiql-testscript/pts/test-scripts/cast/Readme.md
+++
@@ -1,5 +0,0 @@
-# Casting function tests
-
-This directory contains tests related to casting from one type to another in SQL. Test files are separated per 
-target type.
-
--- test/partiql-testscript/src/main/kotlin/org/partiql/testscript/parser/ScriptLocation.kt
+++
@@ -1,7 +0,0 @@
-package org.partiql.testscript.parser
-
-data class ScriptLocation(val inputName: String, val lineNum: Long) {
-    override fun toString(): String {
-        return "$inputName:$lineNum"
-    }
-}
