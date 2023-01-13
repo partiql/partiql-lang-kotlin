f2ef065b011e7189219a9e09c0dc715009c4486a
R. C. Howell <RCHowell@users.noreply.github.com>
2023-01-13 09:14:05 -0800

    Fixes global_env in the shell (#968)

--- partiql-app/partiql-cli/src/main/kotlin/org/partiql/cli/pico/PartiQLCommand.kt
+++ partiql-app/partiql-cli/src/main/kotlin/org/partiql/cli/pico/PartiQLCommand.kt
@@ -90,7 +90,7 @@ internal class PartiQLCommand(private val ion: IonSystem) : Runnable {
         }
         input.use { src ->
             output.use { out ->
-                Cli(ion, src, exec.inputFormat, out, exec.outputFormat, options.pipeline, options.globalEnvironment, queryWithoutShebang, exec.wrapIon).run()
+                Cli(ion, src, exec.inputFormat, out, exec.outputFormat, options.pipeline, options.environment, queryWithoutShebang, exec.wrapIon).run()
                 out.write(System.lineSeparator().toByteArray(Charsets.UTF_8))
             }
         }
@@ -101,7 +101,7 @@ internal class PartiQLCommand(private val ion: IonSystem) : Runnable {
      */
     private fun runShell(shell: ShellOptions = ShellOptions()) {
         val config = Shell.ShellConfiguration(isMonochrome = shell.isMonochrome)
-        Shell(System.out, options.pipeline, options.globalEnvironment, config).start()
+        Shell(System.out, options.pipeline, options.environment, config).start()
     }
 
     /**
