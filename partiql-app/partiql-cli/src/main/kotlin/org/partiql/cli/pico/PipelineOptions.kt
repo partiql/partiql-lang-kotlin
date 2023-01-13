14d9058a64bf1129d12d496544cd6186815554d5
R. C. Howell <howero@amazon.com>
2023-01-13 08:58:05 -0800

    Fixes global_env in the shell

--- partiql-app/partiql-cli/src/main/kotlin/org/partiql/cli/pico/PipelineOptions.kt
+++ partiql-app/partiql-cli/src/main/kotlin/org/partiql/cli/pico/PipelineOptions.kt
@@ -28,22 +28,46 @@ import java.io.File
 
 internal class PipelineOptions {
 
-    @CommandLine.Option(names = ["-p", "--pipeline"], description = ["The type of pipeline to use: [\${COMPLETION-CANDIDATES}]"], paramLabel = "TYPE")
+    @CommandLine.Option(
+        names = ["-p", "--pipeline"],
+        description = ["The type of pipeline to use: [\${COMPLETION-CANDIDATES}]"],
+        paramLabel = "TYPE"
+    )
     var pipelineType: AbstractPipeline.PipelineType = AbstractPipeline.PipelineType.STANDARD
 
-    @CommandLine.Option(names = ["-e", "--environment"], description = ["File containing the global environment"], paramLabel = "FILE")
+    @CommandLine.Option(
+        names = ["-e", "--environment"],
+        description = ["File containing the global environment"],
+        paramLabel = "FILE"
+    )
     var environmentFile: File? = null
 
-    @CommandLine.Option(names = ["--typing-mode"], description = ["Specifies the typing mode: [\${COMPLETION-CANDIDATES}]"], paramLabel = "MODE")
+    @CommandLine.Option(
+        names = ["--typing-mode"],
+        description = ["Specifies the typing mode: [\${COMPLETION-CANDIDATES}]"],
+        paramLabel = "MODE"
+    )
     var typingMode: TypingMode = TypingMode.LEGACY
 
-    @CommandLine.Option(names = ["--typed-op-behavior"], description = ["Indicates how CAST should behave: [\${COMPLETION-CANDIDATES}]"], paramLabel = "OPT")
+    @CommandLine.Option(
+        names = ["--typed-op-behavior"],
+        description = ["Indicates how CAST should behave: [\${COMPLETION-CANDIDATES}]"],
+        paramLabel = "OPT"
+    )
     var typedOpBehavior: TypedOpBehavior = TypedOpBehavior.HONOR_PARAMETERS
 
-    @CommandLine.Option(names = ["--projection-iter-behavior"], description = ["Controls the behavior of ExprValue.iterator in the projection result: [\${COMPLETION-CANDIDATES}]"], paramLabel = "OPT")
+    @CommandLine.Option(
+        names = ["--projection-iter-behavior"],
+        description = ["Controls the behavior of ExprValue.iterator in the projection result: [\${COMPLETION-CANDIDATES}]"],
+        paramLabel = "OPT"
+    )
     var projIterBehavior: ProjectionIterationBehavior = ProjectionIterationBehavior.FILTER_MISSING
 
-    @CommandLine.Option(names = ["-u", "--undefined-variable-behavior"], description = ["Defines the behavior when a non-existent variable is referenced: [\${COMPLETION-CANDIDATES}]"], paramLabel = "OPT")
+    @CommandLine.Option(
+        names = ["-u", "--undefined-variable-behavior"],
+        description = ["Defines the behavior when a non-existent variable is referenced: [\${COMPLETION-CANDIDATES}]"],
+        paramLabel = "OPT"
+    )
     var undefinedVarBehavior: UndefinedVariableBehavior = UndefinedVariableBehavior.ERROR
 
     internal val pipeline: AbstractPipeline
@@ -58,14 +82,11 @@ internal class PipelineOptions {
             return AbstractPipeline.create(options)
         }
 
-    internal val globalEnvironment = when (environmentFile) {
-        null -> Bindings.empty()
-        else -> getEnvironment(environmentFile!!, pipeline)
-    }
-
-    private fun getEnvironment(environmentFile: File, pipeline: AbstractPipeline): Bindings<ExprValue> {
-        val configSource = environmentFile.readText(charset("UTF-8"))
-        val config = pipeline.compile(configSource, EvaluationSession.standard()) as PartiQLResult.Value
-        return config.value.bindings
-    }
+    internal val environment: Bindings<ExprValue>
+        get() {
+            if (environmentFile == null) return Bindings.empty()
+            val configSource = environmentFile!!.readText(charset("UTF-8"))
+            val config = pipeline.compile(configSource, EvaluationSession.standard()) as PartiQLResult.Value
+            return config.value.bindings
+        }
 }
