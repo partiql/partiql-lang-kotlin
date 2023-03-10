07dcf1b110e09291e07fea52071ce6d2451fd476
Vladimir Gapeyev <9939945+vgapeyev@users.noreply.github.com>
2023-03-09 22:47:15 -0800

    Revisit FromSourceAliasVisitorTransform implementation.
    The previous implementation relied on delicate ping-ponging of control between multiple instances of the host (FromSourceAliasVisitorTransform) and helper (InnerFromSourceAliasVisitorTransform) visitors.
    In the amended implementation, the host visitor drives the traversal and calls a fresh instance of the helper visitor in each spot where a transformation is needed.
    
    Both implementations create the same number of helper visitors (one per a FROM clause).
    The new implementation needs only one host visitor, as opposed to a host visitor per subquery.
    Absence of ping-ponging means the "stack" of visitors is at most two, as opposed to growing as the traversal goes deeper into the query, with implications for the lifetime of the involved visitor objects and, therefore, memory usage.

--- partiql-lang/src/main/kotlin/org/partiql/lang/eval/visitors/FromSourceAliasVisitorTransform.kt
+++ partiql-lang/src/main/kotlin/org/partiql/lang/eval/visitors/FromSourceAliasVisitorTransform.kt
@@ -15,27 +15,44 @@ import org.partiql.pig.runtime.SymbolPrimitive
  * If provided with a query that has all of the from source aliases already specified, an exact clone is returned.
  */
 class FromSourceAliasVisitorTransform : VisitorTransformBase() {
+    // When this visitor reaches a top-level FromSource, it transforms it with a fresh instance
+    // of the helper visitor below.
+    // Currently, there are two places where a top-level FromSource can occur: in a SELECT and in a DML statement.
 
+    override fun transformExprSelect_from(node: PartiqlAst.Expr.Select): PartiqlAst.FromSource {
+        val newFrom = super.transformExprSelect_from(node) // this applies the transformation to any eligible subqueries
+        return InnerFromSourceAliasVisitorTransform().transformFromSource(newFrom)
+    }
+
+    override fun transformStatementDml_from(node: PartiqlAst.Statement.Dml): PartiqlAst.FromSource? {
+        val newFrom = super.transformStatementDml_from(node)
+        return newFrom?.let { InnerFromSourceAliasVisitorTransform().transformFromSource(it) }
+    }
+
+    /** The helper visitor is responsible for traversing one top-level FromSource and creating aliases in it.
+     *  Only the [transformFromSource] method is intended to be useful as an entry point.
+     *  The visitor is stateful, maintaining a counter for synthetic aliases,
+     *  so a separate instance is needed for each top level FromSource.
+     */
     private class InnerFromSourceAliasVisitorTransform : VisitorTransformBase() {
         private var fromSourceCounter = 0
 
-        override fun transformFromSourceScan_asAlias(node: PartiqlAst.FromSource.Scan): SymbolPrimitive? {
+        override fun transformFromSourceScan_asAlias(node: PartiqlAst.FromSource.Scan): SymbolPrimitive {
             val thisFromSourceIndex = fromSourceCounter++
             return node.asAlias
                 ?: SymbolPrimitive(node.expr.extractColumnAlias(thisFromSourceIndex), node.extractSourceLocation())
         }
 
-        override fun transformFromSourceUnpivot_asAlias(node: PartiqlAst.FromSource.Unpivot): SymbolPrimitive? {
+        override fun transformFromSourceUnpivot_asAlias(node: PartiqlAst.FromSource.Unpivot): SymbolPrimitive {
             val thisFromSourceIndex = fromSourceCounter++
             return node.asAlias
                 ?: SymbolPrimitive(node.expr.extractColumnAlias(thisFromSourceIndex), node.extractSourceLocation())
         }
 
-        // Need use a different [fromSourceCounter] for sub-queries.
-        override fun transformExprSelect(node: PartiqlAst.Expr.Select): PartiqlAst.Expr =
-            FromSourceAliasVisitorTransform().transformExprSelect(node)
+        // Do not traverse into subexpressions of a [FromSource].
+        // All relevant subqueries are reached by the host [FromSourceAliasVisitorTransform].
+        override fun transformExpr(node: PartiqlAst.Expr): PartiqlAst.Expr {
+            return node
+        }
     }
-
-    override fun transformFromSource(node: PartiqlAst.FromSource): PartiqlAst.FromSource =
-        InnerFromSourceAliasVisitorTransform().transformFromSource(node)
 }
