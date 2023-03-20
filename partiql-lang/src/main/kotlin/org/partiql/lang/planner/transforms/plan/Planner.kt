98359448104d51f7ede22978a3b5491188d6e8b1
Yingtao Liu <yliuuu@amazon.com>
2023-03-20 14:37:39 -0700

    public available Planner.plan

---
+++ partiql-lang/src/main/kotlin/org/partiql/lang/planner/transforms/plan/Planner.kt
@@ -0,0 +1,18 @@
+package org.partiql.lang.planner.transforms.plan
+
+import org.partiql.lang.domains.PartiqlAst
+import org.partiql.lang.planner.transforms.normalize
+import org.partiql.lang.syntax.PartiQLParserBuilder
+import org.partiql.plan.Rex
+
+object Planner {
+    fun plan(query : String): Rex {
+        val parser = PartiQLParserBuilder.standard().build()
+        val ast = parser.parseAstStatement(query)
+        val normalizedAST = ast.normalize() as? PartiqlAst.Statement.Query ?: error("Planner only support query for now.")
+        return when (val expr = normalizedAST.expr){
+            is PartiqlAst.Expr.Select -> RelConverter.convert(expr)
+            else -> RexConverter.convert(expr)
+        }
+    }
+}
