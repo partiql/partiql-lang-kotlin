300830baa54e6e67188219afa99b104d3df150aa
John Ed Quinn <johqunn@amazon.com>
2023-05-15 16:24:25 -0700

    Adds Explicit API Mode to Types Project

--- partiql-types/build.gradle.kts
+++ partiql-types/build.gradle.kts
@@ -22,11 +22,6 @@ dependencies {
     implementation(Deps.ionElement)
 }
 
-// TODO: Make this STRICT. See https://github.com/partiql/partiql-lang-kotlin/issues/1078
-kotlin {
-    explicitApi = null
-}
-
 publish {
     artifactId = "partiql-types"
     name = "PartiQL Types"
