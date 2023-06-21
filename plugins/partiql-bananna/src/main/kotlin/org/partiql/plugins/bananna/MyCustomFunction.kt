1aa644a6e7131d2004dcb0e0d941db0e8c115afe
Yuxiao Tang <yuxtang@amazon.com>
2023-06-21 15:28:43 -0700

    Remove bananna

--- plugins/partiql-bananna/src/main/kotlin/org/partiql/plugins/bananna/MyCustomFunction.kt
+++
@@ -1,9 +0,0 @@
-package org.partiql.plugins.bananna
-
-import org.partiql.spi.functions.CustomInterface
-
-public class MyCustomFunction : CustomInterface {
-    override fun performAction() {
-        println("Action performed by MyCustomFunction")
-    }
-}
