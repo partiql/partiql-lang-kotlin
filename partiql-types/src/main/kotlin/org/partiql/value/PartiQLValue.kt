a393117487b21854ff55ab0243796331ede1a85b
Yuxiao Tang <yuxtang@amazon.com>
2023-07-06 10:18:56 -0700

    ServiceLoader

--- partiql-types/src/main/kotlin/org/partiql/value/PartiQLValue.kt
+++ partiql-types/src/main/kotlin/org/partiql/value/PartiQLValue.kt
@@ -193,7 +193,7 @@ public abstract class DecimalValue : NumericValue<BigDecimal>() {
 }
 
 @PartiQLValueExperimental
-public abstract class Float32Value : ScalarValue<Float> {
+public abstract class Float32Value : NumericValue<Float>() {
 
     override val type: PartiQLValueType = PartiQLValueType.FLOAT32
 
@@ -205,7 +205,7 @@ public abstract class Float32Value : ScalarValue<Float> {
 }
 
 @PartiQLValueExperimental
-public abstract class Float64Value : ScalarValue<Double> {
+public abstract class Float64Value : NumericValue<Double>() {
 
     override val type: PartiQLValueType = PartiQLValueType.FLOAT64
 
