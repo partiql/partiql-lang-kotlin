6d49aaf440648727695bc18fdd4835a05307a6aa
R. C. Howell <howero@amazon.com>
2023-07-07 10:48:50 -0700

    Adds api dep; removes accidental example

--- partiql-ast/src/main/kotlin/org/partiql/ast/Ast.kt
+++ partiql-ast/src/main/kotlin/org/partiql/ast/Ast.kt
@@ -13,33 +13,3 @@ object Ast : AstBaseFactory()
 public abstract class AstBaseFactory : AstFactoryImpl() {
     // internal default overrides here
 }
-
-public abstract class MyFactory : AstBaseFactory() {
-
-    override fun identifierSymbol(symbol: String, caseSensitivity: Identifier.CaseSensitivity): Identifier.Symbol {
-        return ComparableIdentifier(_id(), symbol, caseSensitivity)
-    }
-}
-
-//
-class ComparableIdentifier(
-    override val _id: String,
-    override val symbol: String,
-    override val caseSensitivity: Identifier.CaseSensitivity,
-) : Identifier.Symbol {
-
-    override fun copy(symbol: String, caseSensitivity: Identifier.CaseSensitivity): Identifier.Symbol {
-        TODO("Not yet implemented")
-    }
-
-    override val children: List<AstNode> = emptyList()
-
-    override fun equals(other: Any?): Boolean {
-        if (other == null || other !is Identifier.Symbol) return false // different type
-        if (other === this) return true // same object
-        return when (caseSensitivity) {
-            Identifier.CaseSensitivity.SENSITIVE -> this.symbol == other.symbol
-            Identifier.CaseSensitivity.INSENSITIVE -> this.symbol.lowercase() == other.symbol.lowercase()
-        }
-    }
-}
