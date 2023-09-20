e772bfe7a33fb14f9ebd4daae4d8eac05218f328
Alan Cai <caialan@amazon.com>
2023-09-20 11:06:48 -0700

    Remove libs configuration

--- partiql-lang/build.gradle.kts
+++ partiql-lang/build.gradle.kts
@@ -22,12 +22,6 @@ plugins {
     id(Plugins.publish)
 }
 
-val libs: Configuration by configurations.creating
-
-configurations {
-    api.get().extendsFrom(libs)
-}
-
 // Disabled for partiql-lang project.
 kotlin {
     explicitApi = null
@@ -105,14 +99,4 @@ tasks.processResources {
         include("partiql.ion")
         into("org/partiql/type-domains/")
     }
-}
-
-tasks.jar {
-    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
-    // adds all `libs(project(...))` to the partiql-lang-kotlin jar
-    from(
-        libs.dependencies.filterIsInstance<ProjectDependency>().map {
-            it.dependencyProject.sourceSets.main.get().output.classesDirs
-        }
-    )
 }
