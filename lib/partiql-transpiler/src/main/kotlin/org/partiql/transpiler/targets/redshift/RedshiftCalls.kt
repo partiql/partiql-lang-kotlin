b3f8ff6de060b35f75e44394e242ec4d7a561945
yliuuuu <107505258+yliuuuu@users.noreply.github.com>
2023-09-27 10:36:45 -0700

    expression comparison (#1212)

--- lib/partiql-transpiler/src/main/kotlin/org/partiql/transpiler/targets/redshift/RedshiftCalls.kt
+++ lib/partiql-transpiler/src/main/kotlin/org/partiql/transpiler/targets/redshift/RedshiftCalls.kt
@@ -10,7 +10,9 @@ import org.partiql.transpiler.info
 import org.partiql.transpiler.sql.SqlArgs
 import org.partiql.transpiler.sql.SqlCallFn
 import org.partiql.transpiler.sql.SqlCalls
+import org.partiql.transpiler.warn
 import org.partiql.value.PartiQLValueExperimental
+import org.partiql.value.PartiQLValueType
 import org.partiql.value.symbolValue
 
 @OptIn(PartiQLValueExperimental::class)
@@ -51,6 +53,35 @@ public class RedshiftCalls(private val onProblem: ProblemCallback) : SqlCalls() 
         val id = id("sysdate")
         onProblem.info("PartiQL `utcnow()` was replaced by Redshift `SYSDATE`")
         exprVar(id, Expr.Var.Scope.DEFAULT)
+    }
+
+    /**
+     * Push the negation down if possible.
+     * For example : NOT 1 is NULL -> 1 is NOT NULL.
+     */
+    override fun notFn(args: SqlArgs): Expr = Ast.create {
+        val arg = args.first()
+        when (val expr = arg.expr) {
+            is Expr.Between -> exprBetween(expr.value, expr.from, expr.to, true)
+            is Expr.InCollection -> exprInCollection(expr.lhs, expr.rhs, true)
+            is Expr.IsType -> exprIsType(expr.value, expr.type, true)
+            is Expr.Like -> exprLike(expr.value, expr.pattern, expr.escape, true)
+            else -> super.negFn(args)
+        }
+    }
+
+    /**
+     * As far as the documentation goes, there is no indication that redshift support type assertion other than null.
+     * I.e., var IS NULL is supported, var IS INT2 may not.
+     * Also, there is seemingly no helper function like pg_typeof() in redshift either.
+     * Throwing a warning message if the type assertion is not targeting null type.
+     */
+    override fun isType(type: PartiQLValueType, args: SqlArgs): Expr {
+        when (type) {
+            PartiQLValueType.NULL -> Unit
+            else -> onProblem.warn("Redshift does not support type assertion on ${type.name} ")
+        }
+        return super.isType(type, args)
     }
 
     private fun AstFactory.id(symbol: String) = identifierSymbol(symbol, Identifier.CaseSensitivity.INSENSITIVE)
