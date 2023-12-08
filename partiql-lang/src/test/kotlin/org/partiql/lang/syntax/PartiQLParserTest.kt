124e87f25e38c95ca0666ddebe4ad5ab2fa5b3a8
yliuuuu <107505258+yliuuuu@users.noreply.github.com>
2023-12-08 11:10:34 -0800

    Fix Timestamp Type Parsing Issue. (#1284)
    * fix timestamp type parsing issue

--- partiql-lang/src/test/kotlin/org/partiql/lang/syntax/PartiQLParserTest.kt
+++ partiql-lang/src/test/kotlin/org/partiql/lang/syntax/PartiQLParserTest.kt
@@ -366,6 +366,54 @@ class PartiQLParserTest : PartiQLParserTestBase() {
     )
 
     @Test
+    fun callIsTimestamp() = assertExpression(
+        "t1 IS TIMESTAMP",
+        "(is_type (id t1 (case_insensitive) (unqualified)) (timestamp_type null))"
+    )
+
+    @Test
+    fun callIsTime() = assertExpression(
+        "t1 IS TIME",
+        "(is_type (id t1 (case_insensitive) (unqualified)) (time_type null))"
+    )
+
+    @Test
+    fun callIsTimestampWithTimeZone() = assertExpression(
+        "t1 IS TIMESTAMP WITH TIME ZONE",
+        "(is_type (id t1 (case_insensitive) (unqualified)) (timestamp_with_time_zone_type null))"
+    )
+
+    @Test
+    fun callIsTimeWithTimeZone() = assertExpression(
+        "t1 IS TIME WITH TIME ZONE",
+        "(is_type (id t1 (case_insensitive) (unqualified)) (time_with_time_zone_type null))"
+    )
+
+    @Test
+    fun callIsTimestampWithPrecision() = assertExpression(
+        "t1 IS TIMESTAMP(3)",
+        "(is_type (id t1 (case_insensitive) (unqualified)) (timestamp_type 3))"
+    )
+
+    @Test
+    fun callIsTimeWithPrecision() = assertExpression(
+        "t1 IS TIME(3)",
+        "(is_type (id t1 (case_insensitive) (unqualified)) (time_type 3))"
+    )
+
+    @Test
+    fun callIsTimestampWithTimeZoneAndPrecision() = assertExpression(
+        "t1 IS TIMESTAMP(3) WITH TIME ZONE",
+        "(is_type (id t1 (case_insensitive) (unqualified)) (timestamp_with_time_zone_type 3))"
+    )
+
+    @Test
+    fun callIsTimeWithTimeZoneAndPrecision() = assertExpression(
+        "t1 IS TIME(3) WITH TIME ZONE",
+        "(is_type (id t1 (case_insensitive) (unqualified)) (time_with_time_zone_type 3))"
+    )
+
+    @Test
     fun nullIsNotNull() = assertExpression(
         "null IS NOT NULL",
         "(not (is_type (lit null) (null_type)))"
