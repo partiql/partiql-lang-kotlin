1dabd4e07e2d01681707c31db481e004de4ede51
yliuuuu <107505258+yliuuuu@users.noreply.github.com>
2024-01-12 15:21:37 -0800

    Implements date_add builtin in partiql-eval (#1334)

--- plugins/partiql-plugin/src/main/kotlin/org/partiql/plugin/internal/fn/scalar/FnDateDiffSecond.kt
+++ plugins/partiql-plugin/src/main/kotlin/org/partiql/plugin/internal/fn/scalar/FnDateDiffSecond.kt
@@ -9,29 +9,9 @@ import org.partiql.types.function.FunctionParameter
 import org.partiql.types.function.FunctionSignature
 import org.partiql.value.PartiQLValue
 import org.partiql.value.PartiQLValueExperimental
-import org.partiql.value.PartiQLValueType.DATE
 import org.partiql.value.PartiQLValueType.INT64
 import org.partiql.value.PartiQLValueType.TIME
 import org.partiql.value.PartiQLValueType.TIMESTAMP
-
-@OptIn(PartiQLValueExperimental::class, PartiQLFunctionExperimental::class)
-internal object Fn_DATE_DIFF_SECOND__DATE_DATE__INT64 : PartiQLFunction.Scalar {
-
-    override val signature = FunctionSignature.Scalar(
-        name = "date_diff_second",
-        returns = INT64,
-        parameters = listOf(
-            FunctionParameter("datetime1", DATE),
-            FunctionParameter("datetime2", DATE),
-        ),
-        isNullCall = true,
-        isNullable = false,
-    )
-
-    override fun invoke(args: Array<PartiQLValue>): PartiQLValue {
-        TODO("Function date_diff_second not implemented")
-    }
-}
 
 @OptIn(PartiQLValueExperimental::class, PartiQLFunctionExperimental::class)
 internal object Fn_DATE_DIFF_SECOND__TIME_TIME__INT64 : PartiQLFunction.Scalar {
