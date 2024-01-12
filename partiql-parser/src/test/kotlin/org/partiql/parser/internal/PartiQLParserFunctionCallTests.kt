c0454e7836d9f6b40c2fe83a39844b048f699369
R. C. Howell <howero@amazon.com>
2024-01-12 14:50:08 -0800

    Adds qualifiers to function call expressions

---
+++ partiql-parser/src/test/kotlin/org/partiql/parser/internal/PartiQLParserFunctionCallTests.kt
@@ -0,0 +1,136 @@
+package org.partiql.parser.internal
+
+import org.junit.jupiter.api.Test
+import org.partiql.ast.AstNode
+import org.partiql.ast.Expr
+import org.partiql.ast.Identifier
+import org.partiql.ast.exprCall
+import org.partiql.ast.identifierQualified
+import org.partiql.ast.identifierSymbol
+import org.partiql.ast.statementQuery
+import kotlin.test.assertEquals
+
+class PartiQLParserFunctionCallTests {
+
+    private val parser = PartiQLParserDefault()
+
+    private inline fun query(body: () -> Expr) = statementQuery(body())
+
+    @Test
+    fun callUnqualifiedNonReservedInsensitive() = assertExpression(
+        "foo()",
+        query {
+            exprCall(
+                function = identifierSymbol("foo", Identifier.CaseSensitivity.INSENSITIVE),
+                args = emptyList()
+            )
+        }
+    )
+
+    @Test
+    fun callUnqualifiedNonReservedSensitive() = assertExpression(
+        "\"foo\"()",
+        query {
+            exprCall(
+                function = identifierSymbol("foo", Identifier.CaseSensitivity.SENSITIVE),
+                args = emptyList()
+            )
+        }
+    )
+
+    @Test
+    fun callUnqualifiedReservedInsensitive() = assertExpression(
+        "upper()",
+        query {
+            exprCall(
+                function = identifierSymbol("upper", Identifier.CaseSensitivity.INSENSITIVE),
+                args = emptyList()
+            )
+        }
+    )
+
+    @Test
+    fun callUnqualifiedReservedSensitive() = assertExpression(
+        "\"upper\"()",
+        query {
+            exprCall(
+                function = identifierSymbol("upper", Identifier.CaseSensitivity.SENSITIVE),
+                args = emptyList()
+            )
+        }
+    )
+
+    @Test
+    fun callQualifiedNonReservedInsensitive() = assertExpression(
+        "my_catalog.my_schema.foo()",
+        query {
+            exprCall(
+                function = identifierQualified(
+                    root = identifierSymbol("my_catalog", Identifier.CaseSensitivity.INSENSITIVE),
+                    steps = listOf(
+                        identifierSymbol("my_schema", Identifier.CaseSensitivity.INSENSITIVE),
+                        identifierSymbol("foo", Identifier.CaseSensitivity.INSENSITIVE),
+                    )
+                ),
+                args = emptyList()
+            )
+        }
+    )
+
+    @Test
+    fun callQualifiedNonReservedSensitive() = assertExpression(
+        "my_catalog.my_schema.\"foo\"()",
+        query {
+            exprCall(
+                function = identifierQualified(
+                    root = identifierSymbol("my_catalog", Identifier.CaseSensitivity.INSENSITIVE),
+                    steps = listOf(
+                        identifierSymbol("my_schema", Identifier.CaseSensitivity.INSENSITIVE),
+                        identifierSymbol("foo", Identifier.CaseSensitivity.SENSITIVE),
+                    )
+                ),
+                args = emptyList()
+            )
+        }
+    )
+
+    @Test
+    fun callQualifiedReservedInsensitive() = assertExpression(
+        "my_catalog.my_schema.upper()",
+        query {
+            exprCall(
+                function = identifierQualified(
+                    root = identifierSymbol("my_catalog", Identifier.CaseSensitivity.INSENSITIVE),
+                    steps = listOf(
+                        identifierSymbol("my_schema", Identifier.CaseSensitivity.INSENSITIVE),
+                        identifierSymbol("upper", Identifier.CaseSensitivity.INSENSITIVE),
+                    )
+                ),
+                args = emptyList()
+            )
+        }
+    )
+
+    @Test
+    fun callQualifiedReservedSensitive() = assertExpression(
+        "my_catalog.my_schema.\"upper\"()",
+        query {
+            exprCall(
+                function = identifierQualified(
+                    root = identifierSymbol("my_catalog", Identifier.CaseSensitivity.INSENSITIVE),
+                    steps = listOf(
+                        identifierSymbol("my_schema", Identifier.CaseSensitivity.INSENSITIVE),
+                        identifierSymbol("upper", Identifier.CaseSensitivity.SENSITIVE),
+                    )
+                ),
+                args = emptyList()
+            )
+        }
+    )
+
+    private fun assertExpression(input: String, expected: AstNode) {
+        val result = parser.parse(input)
+        val actual = result.root
+        assertEquals(expected, actual)
+    }
+}
