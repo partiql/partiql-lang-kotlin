c63badbba4be0826201d94442d19be38d2b368dc
Alan Cai <caialan@amazon.com>
2024-04-17 17:37:04 -0700

    Add custom ktlint rules to prevent Java interop issues (#1414)
    (cherry picked from commit 4851cac1343ed041f01a9c5d4c0445ccfd2143fb)

---
+++ custom-ktlint-rules/src/test/kotlin/org/partiql/ktlint/rule/TopLevelInternalRuleTest.kt
@@ -0,0 +1,48 @@
+package org.partiql.ktlint.rule
+
+import com.pinterest.ktlint.core.LintError
+import com.pinterest.ktlint.test.lint
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Test
+
+class TopLevelInternalRuleTest {
+    @Test
+    fun `top-level internal`() {
+        val code =
+            """
+            internal fun internalTopLevelFun() {}   // ktlint error
+            
+            internal val internalTopLevelVal = 123  // ktlint error
+            
+            internal var internalTopLevelVar = 456    // ktlint error
+            
+            // No errors for below (for this rule)
+            public fun publicTopLevelFun() {}
+            
+            public val publicTopLevelVal = 123
+            
+            public var publicTopLevelVar = 456
+            
+            fun publicTopLevelFun2() {}
+            
+            val publicTopLevelVal = 123
+            
+            var publicTopLevelVar = 456
+            
+            public class PublicClass {
+                internal fun internalFun() {}
+            
+                internal val internalVal = 123
+                
+                public fun publicFun() {}
+                
+                public val publicVal = 123
+            }
+            """.trimIndent()
+        Assertions.assertThat(TopLevelInternalRule().lint(code)).containsExactly(
+            LintError(1, 14, "top-level-internal", "Top-level internal declaration found: internalTopLevelFun"),
+            LintError(3, 14, "top-level-internal", "Top-level internal declaration found: internalTopLevelVal"),
+            LintError(5, 14, "top-level-internal", "Top-level internal declaration found: internalTopLevelVar")
+        )
+    }
+}
