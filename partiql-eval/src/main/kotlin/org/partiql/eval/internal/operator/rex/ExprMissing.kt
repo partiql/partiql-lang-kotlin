c87aa1e7bd17659205034f2c05e59c1cc899b518
R. C. Howell <howero@amazon.com>
2024-04-18 17:36:03 -0700

    Promote Rex.Op.Missing to compile time error in strict

---
+++ partiql-eval/src/main/kotlin/org/partiql/eval/internal/operator/rex/ExprMissing.kt
@@ -0,0 +1,17 @@
+package org.partiql.eval.internal.operator.rex
+
+import org.partiql.errors.TypeCheckException
+import org.partiql.eval.internal.Environment
+import org.partiql.eval.internal.operator.Operator
+import org.partiql.value.PartiQLValue
+import org.partiql.value.PartiQLValueExperimental
+
+internal class ExprMissing(
+    private val message: String,
+) : Operator.Expr {
+
+    @OptIn(PartiQLValueExperimental::class)
+    override fun eval(env: Environment): PartiQLValue {
+        throw TypeCheckException(message)
+    }
+}
