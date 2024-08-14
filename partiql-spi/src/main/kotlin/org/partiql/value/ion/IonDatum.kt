527642575047b399878039cf79be3eb36dc78a0a
R. C. Howell <howero@amazon.com>
2024-08-14 11:22:49 -0700

    Adds basic annotation processing to IonDatum

--- partiql-spi/src/main/kotlin/org/partiql/value/ion/IonDatum.kt
+++ partiql-spi/src/main/kotlin/org/partiql/value/ion/IonDatum.kt
@@ -16,7 +16,6 @@ import com.amazon.ionelement.api.ElementType.SYMBOL
 import com.amazon.ionelement.api.ElementType.TIMESTAMP
 import org.partiql.eval.value.Datum
 import org.partiql.eval.value.Field
-import org.partiql.planner.internal.SqlTypes
 import org.partiql.types.PType
 import org.partiql.value.datetime.Date
 import org.partiql.value.datetime.Time
@@ -27,26 +26,33 @@ import java.math.BigInteger
 /**
  * A [Datum] implemented over Ion's [AnyElement].
  */
-public class IonDatum private constructor(value: AnyElement) : Datum {
+public class IonDatum private constructor(value: AnyElement, type: PType) : Datum {
 
     // DO NOT USE FINAL
     private var _value = value
+    private var _type = type
     private var _kind = value.type
-    private var _type = when (value.type) {
-        NULL -> SqlTypes.dynamic()
-        BOOL -> SqlTypes.bool()
-        INT -> SqlTypes.numeric()
-        FLOAT -> SqlTypes.double()
-        DECIMAL -> SqlTypes.decimal()
-        STRING -> SqlTypes.string()
-        CLOB -> SqlTypes.clob()
-        BLOB -> SqlTypes.blob()
-        LIST -> SqlTypes.array()
-        STRUCT -> SqlTypes.struct()
-        // ???
-        SEXP -> PType.typeSexp()
-        SYMBOL -> PType.typeSymbol()
-        TIMESTAMP -> error("PType has no arbitrary timestamp")
+
+    /**
+     * Some encoding of PartiQL values as Ion.
+     */
+    private enum class Annotation(val symbol: String) {
+        MISSING("\$missing"),
+        BAG("\$bag"),
+        DATE("\$date"),
+        TIME("\$time"),
+        TIMESTAMP("\$timestamp"),
+        GRAPH("\$graph");
+
+        override fun toString(): String = symbol
+
+        companion object {
+
+            @JvmStatic
+            fun of(value: AnyElement): Annotation? = value.annotations.lastOrNull()?.let {
+                Annotation.values().find { a -> a.symbol == it }
+            }
+        }
     }
 
     public companion object {
@@ -58,8 +64,72 @@ public class IonDatum private constructor(value: AnyElement) : Datum {
          * @return
          */
         @JvmStatic
-        public fun of(value: AnyElement): IonDatum {
-            return IonDatum(value)
+        public fun of(value: AnyElement): Datum {
+            val tag = Annotation.of(value)
+            val type = when (value.type) {
+                NULL -> return when (tag) {
+                    Annotation.MISSING -> Datum.missing()
+                    Annotation.BAG -> Datum.nullValue(PType.typeBag())
+                    Annotation.DATE -> Datum.nullValue(PType.typeDate())
+                    Annotation.TIME -> Datum.nullValue(PType.typeTimeWithoutTZ(6))
+                    Annotation.TIMESTAMP -> Datum.nullValue(PType.typeTimeWithoutTZ(6))
+                    Annotation.GRAPH -> error("Datum does not support GRAPH type.")
+                    null -> Datum.nullValue()
+                }
+                BOOL -> when (tag) {
+                    null -> PType.typeBool()
+                    else -> error("Unexpected type annotation for Ion BOOL: $tag")
+                }
+                INT -> when (tag) {
+                    null -> PType.typeIntArbitrary()
+                    else -> error("Unexpected type annotation for Ion INT: $tag")
+                }
+                FLOAT -> when (tag) {
+                    null -> PType.typeDoublePrecision()
+                    else -> error("Unexpected type annotation for Ion FLOAT: $tag")
+                }
+                DECIMAL -> when (tag) {
+                    null -> PType.typeDecimalArbitrary()
+                    else -> error("Unexpected type annotation for Ion DECIMAL: $tag")
+                }
+                STRING -> when (tag) {
+                    null -> PType.typeString()
+                    else -> error("Unexpected type annotation for Ion STRING: $tag")
+                }
+                CLOB -> when (tag) {
+                    null -> PType.typeClob(Int.MAX_VALUE)
+                    else -> error("Unexpected type annotation for Ion CLOB: $tag")
+                }
+                BLOB -> when (tag) {
+                    null -> PType.typeBlob(Int.MAX_VALUE)
+                    else -> error("Unexpected type annotation for Ion BLOB: $tag")
+                }
+                LIST -> when (tag) {
+                    Annotation.BAG -> PType.typeBag()
+                    null -> PType.typeList()
+                    else -> error("Unexpected type annotation for Ion LIST: $tag")
+                }
+                STRUCT -> when (tag) {
+                    null -> PType.typeStruct()
+                    Annotation.DATE -> TODO("IonDatum for DATE not supported")
+                    Annotation.TIME -> TODO("IonDatum for TIME not supported")
+                    Annotation.TIMESTAMP -> TODO("IonDatum for TIMESTAMP not supported")
+                    else -> error("Unexpected type annotation for Ion STRUCT: $tag")
+                }
+                SEXP -> when (tag) {
+                    null -> PType.typeSexp()
+                    else -> error("Unexpected type annotation for Ion SEXP: $tag")
+                }
+                SYMBOL -> when (tag) {
+                    null -> PType.typeSymbol()
+                    else -> error("Unexpected type annotation for Ion SYMBOL: $tag")
+                }
+                TIMESTAMP -> when (tag) {
+                    null -> PType.typeTimestampWithoutTZ(6)
+                    else -> error("Unexpected type annotation for Ion TIMESTAMP: $tag")
+                }
+            }
+            return IonDatum(value, type)
         }
     }
 
