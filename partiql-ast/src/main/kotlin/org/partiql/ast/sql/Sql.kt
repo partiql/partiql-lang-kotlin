ec771e46b7cd13f18f3cbe94638dbffb4e175273
R. C. Howell <howero@amazon.com>
2024-08-21 14:23:44 -0700

    Replaces deprecated SqlBlock with optimized version

--- partiql-ast/src/main/kotlin/org/partiql/ast/sql/Sql.kt
+++ partiql-ast/src/main/kotlin/org/partiql/ast/sql/Sql.kt
@@ -1,76 +1,20 @@
 package org.partiql.ast.sql
 
 import org.partiql.ast.AstNode
-import org.partiql.ast.sql.internal.InternalSqlDialect
-import org.partiql.ast.sql.internal.InternalSqlLayout
 
 /**
- * No argument uses optimized internal. Leaving older ones for backwards-compatibility.
+ * Pretty-print this [AstNode] as SQL text with the given (or standard) [SqlLayout] and [SqlDialect].
+ *
+ * @see SqlLayout
+ * @see SqlDialect
  */
-public fun AstNode.sql(): String {
-    val head = InternalSqlDialect.PARTIQL.apply(this)
-    return InternalSqlLayout.format(head)
-}
+@JvmOverloads
+fun AstNode.sql(
+    layout: SqlLayout = SqlLayout.STANDARD,
+    dialect: SqlDialect = SqlDialect.STANDARD,
+): String = dialect.transform(this).sql(layout)
 
 /**
- * Pretty-print this [AstNode] as SQL text with the given [SqlLayout]
+ * Write this [SqlBlock] tree as SQL text with the given [SqlLayout].
  */
-@Deprecated("To be removed in the next major version")
-public fun AstNode.sql(
-    layout: SqlLayout = SqlLayout.DEFAULT,
-): String = SqlDialect.PARTIQL.apply(this).sql(layout)
-
-/**
- * Pretty-print this [AstNode] as SQL text with the given [SqlDialect]
- */
-@Deprecated("To be removed in the next major version")
-public fun AstNode.sql(
-    dialect: SqlDialect = SqlDialect.PARTIQL,
-): String = dialect.apply(this).sql(SqlLayout.DEFAULT)
-
-/**
- * Pretty-print this [AstNode] as SQL text with the given [SqlLayout] and [SqlDialect]
- */
-@Deprecated("To be removed in the next major version")
-public fun AstNode.sql(
-    layout: SqlLayout,
-    dialect: SqlDialect,
-): String = dialect.apply(this).sql(layout)
-
-// a <> b  <-> a concat b
-
-internal infix fun SqlBlock.concat(rhs: SqlBlock): SqlBlock = link(this, rhs)
-
-internal infix fun SqlBlock.concat(text: String): SqlBlock = link(this, text(text))
-
-internal infix operator fun SqlBlock.plus(rhs: SqlBlock): SqlBlock = link(this, rhs)
-
-internal infix operator fun SqlBlock.plus(text: String): SqlBlock = link(this, text(text))
-
-// Shorthand
-
-internal val NIL = SqlBlock.Nil
-
-internal val NL = SqlBlock.NL
-
-internal fun text(text: String) = SqlBlock.Text(text)
-
-internal fun link(lhs: SqlBlock, rhs: SqlBlock) = SqlBlock.Link(lhs, rhs)
-
-internal fun nest(block: () -> SqlBlock) = SqlBlock.Nest(block())
-
-internal fun list(start: String?, end: String?, delimiter: String? = ",", items: () -> List<SqlBlock>): SqlBlock {
-    var h: SqlBlock = NIL
-    h = if (start != null) h + start else h
-    h += nest {
-        val kids = items()
-        var list: SqlBlock = NIL
-        kids.foldIndexed(list) { i, a, item ->
-            list += item
-            list = if (delimiter != null && (i + 1) < kids.size) a + delimiter else a
-            list
-        }
-    }
-    h = if (end != null) h + end else h
-    return h
-}
+fun SqlBlock.sql(layout: SqlLayout = SqlLayout.STANDARD) = layout.print(this)
--- partiql-ast/src/main/kotlin/org/partiql/ast/sql/SqlBlock.kt
+++ partiql-ast/src/main/kotlin/org/partiql/ast/sql/SqlBlock.kt
@@ -1,93 +1,64 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates.  All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ *  You may not use this file except in compliance with the License.
+ * A copy of the License is located at:
+ *
+ *      http://aws.amazon.com/apache2.0/
+ *
+ *  or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific
+ *  language governing permissions and limitations under the License.
+ */
+
 package org.partiql.ast.sql
 
 /**
- * Write this [SqlBlock] tree as SQL text with the given [SqlLayout].
- *
- * @param layout    SQL formatting ruleset
- * @return SQL text
+ * Representation of some textual elements as a token (singly-linked) list.
  */
-@Deprecated("To be removed in the next major version")
-public fun SqlBlock.sql(layout: SqlLayout = SqlLayout.DEFAULT): String = layout.format(this)
+sealed class SqlBlock {
 
-/**
- * Representation of some textual corpus; akin to Wadler's "A prettier printer" Document type.
- */
-@Deprecated("This will be changed in the next major version")
-sealed interface SqlBlock {
+    /**
+     * Next token (if any) in the list.
+     */
+    @JvmField
+    var next: SqlBlock? = null
 
-    public override fun toString(): String
+    /**
+     * An empty [SqlBlock] to be used as a root.
+     */
+    class Nil : SqlBlock()
 
-    public fun <R, C> accept(visitor: BlockVisitor<R, C>, ctx: C): R
+    /**
+     * A newline token.
+     */
+    object Line : SqlBlock()
 
-    public object Nil : SqlBlock {
+    /**
+     * A raw text token. Cannot be broken.
+     */
+    class Text(@JvmField var text: String) : SqlBlock()
 
-        override fun toString() = ""
+    /**
+     * A nest token representing a (possible indented) token sublist.
+     *
+     * @property prefix     A prefix character such as '{', '(', or '['.
+     * @property postfix    A postfix character such as  '}', ')', or ']'.
+     * @property child
+     */
+    class Nest(
+        @JvmField var prefix: String?,
+        @JvmField var postfix: String?,
+        @JvmField var child: SqlBlock,
+    ) : SqlBlock()
 
-        override fun <R, C> accept(visitor: BlockVisitor<R, C>, ctx: C): R = visitor.visitNil(this, ctx)
+    companion object {
+
+        /**
+         * Helper function to create root node (empty).
+         */
+        @JvmStatic
+        internal fun nil(): SqlBlock = Nil()
     }
-
-    public object NL : SqlBlock {
-
-        override fun toString() = "\n"
-
-        override fun <R, C> accept(visitor: BlockVisitor<R, C>, ctx: C): R = visitor.visitNewline(this, ctx)
-    }
-
-    public class Text(val text: String) : SqlBlock {
-
-        override fun toString() = text
-
-        override fun <R, C> accept(visitor: BlockVisitor<R, C>, ctx: C): R = visitor.visitText(this, ctx)
-    }
-
-    public class Nest(val child: SqlBlock) : SqlBlock {
-
-        override fun toString() = child.toString()
-
-        override fun <R, C> accept(visitor: BlockVisitor<R, C>, ctx: C): R = visitor.visitNest(this, ctx)
-    }
-
-    // Use link block rather than linked-list block.next as it makes pre-order traversal trivial
-    public class Link(val lhs: SqlBlock, val rhs: SqlBlock) : SqlBlock {
-
-        override fun toString() = lhs.toString() + rhs.toString()
-
-        override fun <R, C> accept(visitor: BlockVisitor<R, C>, ctx: C): R = visitor.visitLink(this, ctx)
-    }
-}
-
-@Deprecated("This will be changed in the next major version")
-public interface BlockVisitor<R, C> {
-
-    public fun visit(block: SqlBlock, ctx: C): R
-
-    public fun visitNil(block: SqlBlock.Nil, ctx: C): R
-
-    public fun visitNewline(block: SqlBlock.NL, ctx: C): R
-
-    public fun visitText(block: SqlBlock.Text, ctx: C): R
-
-    public fun visitNest(block: SqlBlock.Nest, ctx: C): R
-
-    public fun visitLink(block: SqlBlock.Link, ctx: C): R
-}
-
-@Deprecated("This will be changed in the next major version")
-public abstract class BlockBaseVisitor<R, C> : BlockVisitor<R, C> {
-
-    public abstract fun defaultReturn(block: SqlBlock, ctx: C): R
-
-    public open fun defaultVisit(block: SqlBlock, ctx: C) = defaultReturn(block, ctx)
-
-    public override fun visit(block: SqlBlock, ctx: C): R = block.accept(this, ctx)
-
-    public override fun visitNil(block: SqlBlock.Nil, ctx: C): R = defaultVisit(block, ctx)
-
-    public override fun visitNewline(block: SqlBlock.NL, ctx: C): R = defaultVisit(block, ctx)
-
-    public override fun visitText(block: SqlBlock.Text, ctx: C): R = defaultVisit(block, ctx)
-
-    public override fun visitNest(block: SqlBlock.Nest, ctx: C): R = defaultVisit(block, ctx)
-
-    public override fun visitLink(block: SqlBlock.Link, ctx: C): R = defaultVisit(block, ctx)
 }
