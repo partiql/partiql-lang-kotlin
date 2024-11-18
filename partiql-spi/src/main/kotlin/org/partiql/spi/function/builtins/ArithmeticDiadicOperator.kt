4c9bc8550117478f0db026008621f41a1d3ce894
John Ed Quinn <johqunn@amazon.com>
2024-11-18 15:39:52 -0800

    Adds an internal TypePrecedence object in SPI

--- partiql-spi/src/main/kotlin/org/partiql/spi/function/builtins/ArithmeticDiadicOperator.kt
+++ partiql-spi/src/main/kotlin/org/partiql/spi/function/builtins/ArithmeticDiadicOperator.kt
@@ -2,6 +2,7 @@ package org.partiql.spi.function.builtins
 
 import org.partiql.spi.function.Function
 import org.partiql.spi.function.Parameter
+import org.partiql.spi.function.builtins.TypePrecedence.TYPE_PRECEDENCE
 import org.partiql.spi.internal.SqlTypeFamily
 import org.partiql.spi.value.Datum
 import org.partiql.types.PType
