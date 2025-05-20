bb84b9ab22b9c96c80dd58f139c79b4fd1049fa1
Alan Cai <caialan@amazon.com>
2025-05-20 10:29:41 -0700

    fix: typing of Rex nodes in the plan (#1757)

---
+++ partiql-planner/src/test/kotlin/org/partiql/planner/internal/typer/InternalToPublicPlanTest.kt
@@ -0,0 +1,33 @@
+package org.partiql.planner.internal.typer
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.partiql.parser.PartiQLParser
+import org.partiql.plan.Action.Query
+import org.partiql.plan.rex.RexBag
+import org.partiql.plan.rex.RexType
+import org.partiql.planner.PartiQLPlanner
+import org.partiql.spi.catalog.Session
+import org.partiql.spi.types.PType
+import kotlin.test.Test
+
+/**
+ * Tests for converting the internal PartiQL plan to the public PartiQL plan.
+ */
+class InternalToPublicPlanTest {
+    val parser = PartiQLParser.standard()
+    val planner = PartiQLPlanner.standard()
+
+    // Test that the types are converted correctly from the internal plan to the public plan.
+    @Test
+    fun testCollectionArgs() {
+        val parserResult = parser.parse("<<1>>")
+        val plannerResult = planner.plan(parserResult.statements.first(), Session.empty())
+        val rex = ((plannerResult.plan.action) as Query).rex
+
+        assertEquals(rex.type, RexType.of(PType.bag(PType.integer())))
+
+        rex as RexBag
+        val firstElement = rex.values.first()
+        assertEquals(firstElement.type, RexType.of(PType.integer()))
+    }
+}
