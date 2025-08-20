f401b78d97b6d1da8c4ad66864d904a4b4834a0b
Di Xiao <diwxiao@amazon.com>
2025-08-20 09:26:51 -0700

    add exception handling for 0.14.9

--- docs/upgrades/v0.14-to-v1-upgrade/examples/src/test/kotlin/examples/ErrorHandlingChanges.kt
+++ docs/upgrades/v0.14-to-v1-upgrade/examples/src/test/kotlin/examples/ErrorHandlingChanges.kt
@@ -1,67 +1,66 @@
 package examples
 
 import org.junit.jupiter.api.assertThrows
-import org.partiql.lang.eval.EvaluationSession
 import org.partiql.parser.PartiQLParser
 import org.partiql.parser.PartiQLParserException
 import org.partiql.parser.SourceLocation
 import org.partiql.planner.PartiQLPlanner
-import java.util.*
+import java.util.Random
 import kotlin.test.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertTrue
 
 class ErrorHandlingChanges {
-
+    /**
+     * The example shows when using {@link PartiQLParser} to parse string statement,
+     * the {@link PartiQLParserException} will be thrown. The parser will stop and throw the exception
+     *  when an error occurs.
+     */
     @Test
-    fun `Parser syntax error handling`() {
+    fun `Parser error handling`() {
         val parser = PartiQLParser.default()
 
         val exception = assertThrows<PartiQLParserException> {
-            parser.parse("SELECT * FRM abd")
+            try {
+                val parseResult = parser.parse("Create Table 'mytable")
+            } catch (e: PartiQLParserException) {
+                // rethrow for exception validation in test
+                throw e
+            }
         }
 
-        assertTrue { exception.message.contains("mismatched input 'FRM'") }
-        assertEquals(SourceLocation(1, 10, 52, 3), exception.location)
-    }
-
-    @Test
-    fun `Planner function type mismatch error`() {
-        val parser = PartiQLParser.default()
-        val planner = PartiQLPlanner.default()
-        val sess = PartiQLPlanner.Session(
-            queryId = UUID.randomUUID().toString(),
-            userId = "debug"
-        )
-
-        val parseResult = parser.parse("SELECT UPPER(123)")
-        val exception = assertThrows<PartiQLParserException> {
-            val result = planner.plan(parseResult.root, sess).plan
-        }
-
-        assertTrue { exception.message.contains("extraneous input '''") }
+        assertTrue { exception.message.contains("extraneous input") }
+        assertEquals("UNRECOGNIZED", exception.tokenType)
         assertEquals(SourceLocation(1, 14, 62, 1), exception.location)
     }
 
+    /**
+     * The example shows when using {@link PartiQLPlanner} to make plan from AST.
+     * There is no specific exception defined in the planner, so the planner will throw any exception
+     *  when an error occurs.
+     */
     @Test
-    fun `Expression evaluation error`() {
-
+    fun `PLanner evaluation error`() {
         val parser = PartiQLParser.default()
         val planner = PartiQLPlanner.default()
-        val sess = PartiQLPlanner.Session(
-            queryId = UUID.randomUUID().toString(),
-            userId = "debug"
+        val session = PartiQLPlanner.Session(
+            queryId = Random().nextInt().toString(),
+            userId = "test-user",
+            currentCatalog = "default",
+            catalogs = mapOf()
         )
 
-        val session = EvaluationSession.builder().build()
-
-        val parseResult = parser.parse("SELECT UPPER(123)")
-        val exception = assertThrows<PartiQLParserException> {
-            val result = planner.plan(parseResult.root, sess).plan
-            result
+        val exception = assertThrows<Exception> {
+            try {
+                val parseResult = parser.parse("Create Table mytable")
+                val planResult = planner.plan(parseResult.root, session)
+            } catch (e: Exception) {
+                // PartiQLParserException will be thrown during PartiQL process the query in the major PartiQL components
+                throw e
+            }
         }
 
-        assertTrue { exception.message.contains("extraneous input '''") }
-        assertEquals(SourceLocation(1, 14, 62, 1), exception.location)
+        assertTrue(exception is IllegalArgumentException)
+        assertTrue(exception.message!!.contains("Unsupported statement"))
     }
 }
--- docs/upgrades/v0.14-to-v1-upgrade/upgraded-examples/src/test/kotlin/examples/ErrorHandlingChanges.kt
+++ docs/upgrades/v0.14-to-v1-upgrade/upgraded-examples/src/test/kotlin/examples/ErrorHandlingChanges.kt
@@ -13,158 +13,151 @@ import org.partiql.spi.errors.PErrorKind
 import org.partiql.spi.errors.PErrorListener
 import org.partiql.spi.errors.PRuntimeException
 import org.partiql.spi.errors.Severity
+import org.partiql.spi.types.PType
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
 class ErrorHandlingChanges {
-    class CustomPErrorListener : PErrorListener {
-        val errorCollection: MutableList<PError> = mutableListOf()
 
-        override fun report(error: PError) {
-            when (error.severity.code()) {
-                // You may choose to customize the error message and then halt the flow by throwing the error, or you can record the error and then throw when it reaches maximum count
-                // In this case, we record only without throwing
-                Severity.ERROR -> errorCollection.add(error)
-                // You may choose to customize the error message, treat warning as an error and then halt the flow by throwing, or you can suppress all warnings.
-                // In this case, we record only without throwing
-                Severity.WARNING -> errorCollection.add(error)
-                else -> error("This shouldn't have occurred.")
-            }
-        }
-    }
-
-    private lateinit var context: Context
-    private lateinit var listener: CustomPErrorListener
-
+    /**
+     * The example shows the default behavior of error handling in major PartiQL components {@link PartiQLParser},
+     * {@link PartiQLPlanner} and {@link PartiQLCompiler}. {@link PRuntimeException} will be thrown from the PartiQL system
+     * to trap any parsing errors or warnings. {@link PRuntimeException} wraps a {@link PError} which includes {@link PError#code()},
+     * {@link PError#kind}, {@link PError#severity}, {@link PError#location} and {@link PError#properties}.
+     * {@link PRuntimeException} with some enhanced information allows us for more robust and customized error control.
+     */
     @Test
-    fun `PartiQLParser error handling`() {
+    fun `Default PartiQL error handling`() {
+        val compiler = PartiQLCompiler.standard()
         val parser = PartiQLParser.standard()
+        val planner = PartiQLPlanner.standard()
+        val session = Session.empty()
 
-        // The PRuntimeException is expected to throw when .parse called on a PartiQL query with invalid syntax.
+        val query = """
+            CREATE TABLE 'mytable;
+        """.trimIndent()
+
         val exception = assertThrows<PRuntimeException> {
-            // An invalid single quote is added before Table name foo
-            parser.parse("CREATE TABLE 'foo")
+            try {
+                val parseResult = parser.parse(query)
+                val plan = planner.plan(parseResult.statements[0], session).plan
+                val result = compiler.prepare(plan, Mode.STRICT())
+            } catch (e: PRuntimeException) {
+                // The exception should have been thrown from PartiQL. You can add your handling here.
+                // Rethrow in test for exception validation.
+                throw e
+            }
         }
 
         assertEquals(exception.error.code(), PError.UNEXPECTED_TOKEN)
         assertEquals(PErrorKind.SYNTAX, exception.error.kind.code())
         assertEquals(Severity.ERROR, exception.error.severity.code())
         assertEquals(SourceLocation(1, 14, 1), exception.error.location)
+        // Inspect properties from exception
+        assertEquals("UNRECOGNIZED", exception.error.get("TOKEN_NAME", String::class.java))
     }
 
+    /**
+     * The example shows we registered a custom error handling in the context of major PartiQL components {@link PartiQLParser},
+     * {@link PartiQLPlanner} and {@link PartiQLCompiler} which records the errors or warning instead of throwing for deferred error processing.
+     * You can choose to treat warning as errors, or inhibit certain warnings or even ignore warnings.
+     * It might be a good idea for you to consolidate your logic of converting PErrors to user-presented messages and use that here.
+     */
     @Test
     fun `PartiQLParser with custom error handler`() {
-        // Register Error Listener
-        listener = CustomPErrorListener()
-        context = Context.of(listener)
+        class CustomPErrorListener : PErrorListener {
+            val errorCollection: MutableList<PError> = mutableListOf()
 
+            override fun report(error: PError) {
+                when (error.severity.code()) {
+                    // You may choose to customize the error message and then halt the flow by throwing the error, or you can record the error and then throw when it reaches maximum count
+                    // In this case, we record only without throwing
+                    Severity.ERROR -> errorCollection.add(error)
+                    // You may choose to customize the error message, treat warning as an error and then halt the flow by throwing, or you can suppress all warnings.
+                    // In this case, we record only without throwing
+                    Severity.WARNING -> errorCollection.add(error)
+                    else -> error("This shouldn't have occurred.")
+                }
+            }
+        }
+
+        // Register Error Listener
+        val listener = CustomPErrorListener()
+        val context = Context.of(listener)
+
+        val compiler = PartiQLCompiler.standard()
         val parser = PartiQLParser.standard()
-        val parseResult: PartiQLParser.Result
+        val planner = PartiQLPlanner.standard()
+        val session = Session.empty()
+
+        val query = """
+            CREATE TABLE mytable;
+            INSERT INTO 'mytable VALUES (2,3);
+        """.trimIndent()
+
+        // The example shows you how to implement custom error handling for PartiQL. In the example, we will record the
+        // exception for delayed processing to override the default behavior.
         try {
-            // An invalid single quote is added before Table name foo
-            parseResult = parser.parse("CREATE TABLE 'mytable", context)
-        } catch (ex: PRuntimeException) {
-            // Since we register custom error handler to record all errors instead of throwing, these exceptions were likely unexpected.
-            // However, the PartiQL library may throw PRuntimeExceptions as well. You should be able to unwrap these exceptions and provide quality
-            // error messages to your users. In this case, we throw
-            throw ex
+            // Parser with custom context
+            val parseResult = parser.parse(query, context)
+            // Planner with custom context
+            val plan = planner.plan(parseResult.statements[0], session, context).plan
+            // Complier with custom context
+            val result = compiler.prepare(plan, Mode.STRICT(), context)
+        } catch (e: PRuntimeException) {
+            // This will not throw as we register custom error handling, However, it is still good practice to catch this exception.
+            // PartiQL system may still throw PRuntimeException, you may want to print user friendly message.
+            throw e
         }
 
         // Process your listener now as the errors are not thrown in this example.
-        val exception = listener.errorCollection[0]
+        assertEquals(2, listener.errorCollection.size)
 
-        assertEquals(PError.UNEXPECTED_TOKEN, exception.code())
-        assertEquals(PErrorKind.SYNTAX, exception.kind.code())
-        assertEquals(Severity.ERROR, exception.severity.code())
-        assertEquals(SourceLocation(1, 14, 1), exception.location)
+        val exception1 = listener.errorCollection[0]
+
+        assertEquals(PError.UNEXPECTED_TOKEN, exception1.code())
+        assertEquals(PErrorKind.SYNTAX, exception1.kind.code())
+        assertEquals(Severity.ERROR, exception1.severity.code())
+        assertEquals(SourceLocation(2, 13, 1), exception1.location)
+        // Inspect properties from exception
+        assertEquals("UNRECOGNIZED", exception1.get("TOKEN_NAME", String::class.java))
+
+        val exception2 = listener.errorCollection[1]
+
+        assertEquals(PError.INTERNAL_ERROR, exception2.code())
+        assertEquals(PErrorKind.SEMANTIC, exception2.kind.code())
+        assertEquals(Severity.ERROR, exception2.severity.code())
+        assertEquals("CREATE TABLE has not been supported yet in AstRewriter", exception2.get("CAUSE", Throwable::class.java)!!.message)
     }
 
+    /**
+     *  Error listeners are specifically meant to provide control over the reporting of errors for PartiQL’s
+     *  major components (parser, planner, and compiler). However, for the execution of compiled statements,
+     *  Instead, it immediately throws PRuntimeException.
+     */
     @Test
-    fun `PartiQLPlanner error handling`() {
-        val parser = PartiQLParser.standard()
-        val planner = PartiQLPlanner.standard()
-        val session = Session.empty()
-
-        val parseResult = parser.parse("CREATE TABLE mytable")
-        val exception = assertThrows<PRuntimeException> {
-            planner.plan(parseResult.statements[0], session)
-        }
-
-        assertEquals(PError.INTERNAL_ERROR, exception.error.code())
-        assertEquals(PErrorKind.SEMANTIC, exception.error.kind.code())
-        assertEquals(Severity.ERROR, exception.error.severity.code())
-    }
-
-    @Test
-    fun `PartiQLPlanner with custom error handler`() {
-        // Register Error Listener
-        listener = CustomPErrorListener()
-        context = Context.of(listener)
-
-        val parser = PartiQLParser.standard()
-        val planner = PartiQLPlanner.standard()
-        val session = Session.empty()
-
-        val parseResult = parser.parse("CREATE TABLE mytable")
-
-        try {
-            planner.plan(parseResult.statements[0], session, context)
-        } catch (ex: PRuntimeException) {
-            throw ex
-        }
-
-        val exception = listener.errorCollection[0]
-
-        assertEquals(PError.INTERNAL_ERROR, exception.code())
-        assertEquals(PErrorKind.SEMANTIC, exception.kind.code())
-        assertEquals(Severity.ERROR, exception.severity.code())
-    }
-
-    @Test
-    fun `PartiQLCompiler error handling`() {
+    fun `PartiQL statement execution error handling`() {
         val compiler = PartiQLCompiler.standard()
         val parser = PartiQLParser.standard()
         val planner = PartiQLPlanner.standard()
+        val session = Session.empty()
 
         val parseResult = parser.parse("1 / 0")
-
-        val session = Session.empty()
         val plan = planner.plan(parseResult.statements[0], session).plan
+        val statement = compiler.prepare(plan, Mode.STRICT())
         val exception = assertThrows<PRuntimeException> {
-            val result = compiler.prepare(plan, Mode.STRICT()).execute()
+            try {
+                statement.execute()
+            } catch (e: PRuntimeException) {
+                // A DIVISION_BY_ZERO will throw during the execution
+                throw e
+            }
         }
 
-        assertEquals(PError.DIVISION_BY_ZERO, exception.error.code())
+        assertEquals(exception.error.code(), PError.DIVISION_BY_ZERO)
         assertEquals(PErrorKind.EXECUTION, exception.error.kind.code())
         assertEquals(Severity.ERROR, exception.error.severity.code())
+        assertEquals("1", exception.error.get("DIVIDEND", String::class.java))
+        assertEquals(PType.integer(), exception.error.get("DIVIDEND_TYPE", PType::class.java))
     }
-
-    @Test
-    fun `PartiQLCompiler with custom error handler`() {
-        // Register Error Listener
-        listener = CustomPErrorListener()
-        context = Context.of(listener)
-
-        val compiler = PartiQLCompiler.standard()
-        val parser = PartiQLParser.standard()
-        val planner = PartiQLPlanner.standard()
-
-        val parseResult = parser.parse("1 / 0")
-
-        val session = Session.empty()
-        val plan = planner.plan(parseResult.statements[0], session).plan
-        try {
-            val result = compiler.prepare(plan, Mode.STRICT(), context)
-        } catch (ex: PRuntimeException) {
-            throw ex
-        }
-
-        val exception = listener.errorCollection[0]
-
-        assertEquals(PError.DIVISION_BY_ZERO, exception.code())
-        assertEquals(PErrorKind.EXECUTION, exception.kind.code())
-        assertEquals(Severity.ERROR, exception.severity.code())
-    }
-
-
 }
