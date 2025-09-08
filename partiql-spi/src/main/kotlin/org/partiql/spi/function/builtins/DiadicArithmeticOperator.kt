b1e79147276ff64a8413e4c7a3774c82c2702d53
Di Xiao <diwxiao@amazon.com>
2025-09-08 15:41:04 -0700

    minor

--- partiql-spi/src/main/kotlin/org/partiql/spi/function/builtins/DiadicArithmeticOperator.kt
+++ partiql-spi/src/main/kotlin/org/partiql/spi/function/builtins/DiadicArithmeticOperator.kt
@@ -1,6 +1,7 @@
 package org.partiql.spi.function.builtins
 
 import org.partiql.spi.function.Fn
+import org.partiql.spi.internal.SqlTypeFamily
 import org.partiql.spi.types.PType
 
 /**
@@ -13,6 +14,23 @@ internal abstract class DiadicArithmeticOperator(name: String, hidesName: Boolea
     PType.doublePrecision(),
     hidesName = hidesName
 ) {
+    override fun getUnknownInstance(lhs: PType, rhs: PType): Fn? {
+        // The base getUnknownInstance applies to all PTypes with unknown combinations.
+        // Override the base to apply to PTypes, which arithmetic operator supports,
+        // otherwise it will break type mismatch check for unsupported types e.g.
+        // mod('string', null) should give type
+        val allowPTypes = SqlTypeFamily.NUMBER.members +
+                SqlTypeFamily.DATETIME.members +
+                SqlTypeFamily.INTERVAL.members +
+                setOf(PType.UNKNOWN)
+
+        if (lhs.code() in allowPTypes && rhs.code() in allowPTypes) {
+            return super.getUnknownInstance(lhs, rhs)
+        }
+
+        return null
+    }
+
     override fun getUnknownInstance(): Fn? {
         return getDoubleInstance(PType.doublePrecision(), PType.doublePrecision())
     }
--- partiql-spi/src/main/kotlin/org/partiql/spi/internal/SqlTypeFamily.kt
+++ partiql-spi/src/main/kotlin/org/partiql/spi/internal/SqlTypeFamily.kt
@@ -101,6 +101,24 @@ internal class SqlTypeFamily private constructor(
         )
 
         @JvmStatic
-        val DATE = SqlTypeFamily(preferred = PType.date(), members = setOf(PType.DATE))
+        val DATETIME = SqlTypeFamily(
+            preferred = PType.date(),
+            members = setOf(
+                PType.DATE,
+                PType.TIME,
+                PType.TIMEZ,
+                PType.TIMESTAMP,
+                PType.TIMESTAMPZ
+            )
+        )
+
+        @JvmStatic
+        val INTERVAL = SqlTypeFamily(
+            preferred = PType.date(),
+            members = setOf(
+                PType.INTERVAL_YM,
+                PType.INTERVAL_DT
+            )
+        )
     }
 }
