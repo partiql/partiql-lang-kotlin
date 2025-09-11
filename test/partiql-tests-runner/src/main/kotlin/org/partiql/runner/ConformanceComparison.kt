5be058ffe5e0bff8b435c5e9461a8c07633ff9d0
Di Xiao <diwxiao@amazon.com>
2025-09-11 14:49:56 -0700

    minor

--- test/partiql-tests-runner/src/main/kotlin/org/partiql/runner/ConformanceComparison.kt
+++ test/partiql-tests-runner/src/main/kotlin/org/partiql/runner/ConformanceComparison.kt
@@ -29,14 +29,23 @@ fun loadReportFile(file: File, engine: String, commitId: String): Report {
 fun loadReport(reportContent: String, engine: String, commitId: String): Report {
     val reader: IonReader = IonReaderBuilder.standard().build(reportContent)
     val report = Report(engine, commitId, mutableMapOf())
-    reader.next()
-    reader.stepIn()
 
-    while (reader.next() != null) {
-        val tag = reader.fieldName
-        readTestResult(reader).let { report.testsResults[tag] = it }
+    if (!reportContent.contains("partiql-extended")){
+        // Since old report is in old format, we need to read it differently.
+        // Read old report to show better view of new report.
+        // TODO, remove this after new report in next PR.
+        reader.next()
+        readTestResult(reader).let { report.testsResults["partiql"] = it }
+    } else {
+        reader.next()
+        reader.stepIn()
+
+        while (reader.next() != null) {
+            val tag = reader.fieldName
+            readTestResult(reader).let { report.testsResults[tag] = it }
+        }
+        reader.stepOut()
     }
-    reader.stepOut()
     reader.close()
     return report
 }
